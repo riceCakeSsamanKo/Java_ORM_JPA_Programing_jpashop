a8c68460b8c949017c2451b8d83b5e3640339e8b
Min Jeon <mulsoap0504@gmail.com>
2023-03-04 01:25:12 +0900

    jpashop

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,6 +17,8 @@ public class Order {
 
     @Column(name = "MEMBER_ID")
     private Long memberId;
+    private Member member;
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)  //OrderStatus는 enum이고 숫자가 아닌 String으로 식별함
